38782cecb936ff306f6803cb71e996c25b3fdd37
KevinKaras <kevinkaras2@gmail.com>
2022-04-14 12:12:45 -0400

    added correct searchbar divs, containers, img/image container divs"

--- FrontEnd/src/components/NavBar.js
+++ FrontEnd/src/components/NavBar.js
@@ -8,13 +8,22 @@ function NavBar(){
 
     return (
         <div className="NavBarDiv">
+            <div className="Icon-Container">
+                <img className="Site-Logo"></img>
+                <div className="Title-Container">
+                    <div className="Title">Kevin's Keyboards</div>
+                </div>
+            </div>
             <div className="Search-Bar-Container">
                 <Search className="Search-Bar"/>
             </div>
-            <div className="Icon-Container">
-                <img className="Site-Logo"></img>
+            <div className="Button-Container">
+                <button className="-Button"></button>
+                <button className="-Button"></button>
+                <button className="-Button"></button>
+                <button className="-Button"></button>
+                <button className="-Button"></button>
             </div>
-
 
 
         </div>
