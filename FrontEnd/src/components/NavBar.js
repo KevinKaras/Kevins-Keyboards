6dd4cc85e5a54bb11d62e629cbf135f8158dc109
KevinKaras <kevinkaras2@gmail.com>
2022-04-16 12:12:45 -0400

    nav bar buttons, styling, logo, title all added and correct

--- FrontEnd/src/components/NavBar.js
+++ FrontEnd/src/components/NavBar.js
@@ -8,23 +8,26 @@ function NavBar(){
 
     return (
         <div className="Nav-Bar-Div">
-            <div className="Icon-Container">
-                <img className="Site-Logo"></img>
+            <div className="Left-Nav-Side">
+                <div className="Icon-Container">
+                    <img className="Site-Logo"></img>
+                </div>
                 <div className="Title-Container">
                     <div className="Title">Kevin's Keyboards</div>
                 </div>
+                <div className="Search-Bar-Container">
+                    <Search className="Search-Bar"/>
+                </div>
             </div>
-            <div className="Search-Bar-Container">
-                <Search className="Search-Bar"/>
+            <div className="Right-Nav-Side">
+                <div className="Button-Container">
+                    <button className="Shop-Button">Shop</button>
+                    <button className="Discover-Button">Discover</button>
+                    <button className="Login-Button">Login</button>
+                    <button className="My-Cart-Button">My Cart</button>
+                    <button className="Support-Button">Support</button>
+                </div>
             </div>
-            <div className="Button-Container">
-                <button className="Shop-Button">Shop</button>
-                <button className="Discover-Button">Discover</button>
-                <button className="Login-Button">Login</button>
-                <button className="My-Cart-Button">My Cart</button>
-                <button className="Support-Button">Support</button>
-            </div>
-
 
         </div>
         
