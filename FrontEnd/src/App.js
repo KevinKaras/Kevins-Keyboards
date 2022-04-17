32da5a045b6c5991b5af6ef59e22e6afd21b86f3
KevinKaras <kevinkaras2@gmail.com>
2022-04-17 12:12:45 -0400

    saving before implementing redux store

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,11 +1,35 @@
 import React from "react"
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+  } from "react-router-dom";
 import NavBar from "./components/NavBar.js"
+
 
 
 function App(){
 
     return (
-       <NavBar />  
+        <>
+            <NavBar />  
+            <Router>
+                <Routes>
+                    <Route path="">
+
+                    </Route>
+                    <Route>
+                        
+                    </Route>
+                    <Route>
+                        
+                    </Route>
+                    <Route>
+                        
+                    </Route>
+                </Routes>
+            </Router>
+        </>
         
     )
 }
