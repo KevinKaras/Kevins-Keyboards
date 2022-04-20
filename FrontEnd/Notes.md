ef73310d6429a26cba5e750ccaf7993e973749d3
KevinKaras <kevinkaras2@gmail.com>
2022-04-20 12:12:45 -0400

    added notes file to hold model ideas, and added a single fake modelto test

---
+++ FrontEnd/Notes.md
@@ -0,0 +1,14 @@
+
+
+
+
+Model for Item.
+
+
+{
+    "id":           "INTEGER"
+    "Name":         "VARCHAR(25)",
+    "Description":  "VARCHAR(500)",
+    "Price":        "INTEGER",
+    "Image":        "VARCHAR(75)"             //AMAZON S3 LINK
+}
--- FrontEnd/src/features/Shelves/Shelf.js
+++ FrontEnd/src/features/Shelves/Shelf.js
@@ -2,7 +2,15 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const ShelfSlice = createSlice({
     name: "Shelf",
-    initialState: [],
+    initialState: [
+        {
+            "Id":           0,
+            "Name":         "Kevin's Keyboard",
+            "Description":  "This is Kevin's Original Keyboard, it comes with cherry MX brown keyswitches, custom black key-caps with no inscriptions ",
+            "Price":        "100.00",
+            "Image":        "https://customkeyboard.net/wp-content/uploads/2019/10/FC660C-Black-e1572087289431.jpg.webp" 
+        }
+    ],
     reducers: {
         add: state =>{},
         remove: state =>{},
